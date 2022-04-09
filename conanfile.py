53695c317518c608d2722b1205a6b25824539ded
Dylan <2894220@gmail.com>
2022-04-09 16:40:29 +0800

    feat: deploy Qt project
    Signed-off-by: Dylan <2894220@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -7,12 +7,18 @@ class ModuleConan(ConanFile):
     description = "An example for Qt with Conan"
     settings = "os", "compiler", "build_type", "arch"
     generators = "qt", "cmake", "cmake_find_package_multi", "cmake_paths"
-    default_options = "qt:shared=True"
+    default_options = {
+        "qt:shared": True,
+        "qt:qttools": True
+    }
 
     def configure(self):
         del self.settings.compiler.cppstd
 
     def requirements(self):
-        self.requires("qt/6.2.4")
-        self.requires("openssl/1.1.1m")
-        self.requires("harfbuzz/4.2.0")
+        if platform.system() == "Windows":
+            self.requires("qt/5.15.3")
+        else:
+            self.requires("qt/6.2.4")
+            self.requires("harfbuzz/4.2.0")
+            self.requires("openssl/1.1.1n")
